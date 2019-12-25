45e294b5a160d07436cb138422a5751a6f9392fb
SabinoRM <sabino.rendon.mireles@outlook.es>
2019-12-24 18:20:06 -0600

    empezando

--- Master/Rutas/usuarios.js
+++ Master/Rutas/usuarios.js
@@ -5,10 +5,10 @@ var auth0 = require('../Middleware/auth0');
 var router = express.Router();
 
 // Rutas útiles
-router.post('/usuario/guardar', auth0, UsuariosController.save);
+/* router.post('/usuario/guardar', auth0, UsuariosController.save);
 router.get('/usuarios/listarA/:last?', auth0,  UsuariosController.getUsuariosA);
 router.get('/usuarios/listarI/:last?',  auth0, UsuariosController.getUsuariosI);
 router.get('/usuario/buscar/:id',  auth0, UsuariosController.getUsuario);
 router.get('/usuarios/filtrar/:search',  auth0, UsuariosController.search);
-router.put('/usuarios/desactivar/:id',  auth0, UsuariosController.hide);
+router.put('/usuarios/desactivar/:id',  auth0, UsuariosController.hide); */
 module.exports = router;
