e27e8a577f9de4b05d93f368f17d9a09846b1c12
Alejandro Rendón Mireles <Sabino.Rendon.Mireles@outlook.es>
2020-02-05 14:18:52 -0600

    Test Heroku

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = require('./app');
 const port = process.env.PORT || 3000;
 mongoose.set('useFindAndModify', false);
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb+srv://srendon:20141530@cluster0-hjkav.azure.mongodb.net/Pro35?retryWrites=true&w=majority',{useNewUrlParser:true , useFindAndModify: false, useCreateIndex: true, useUnifiedTopology: true })
+mongoose.connect(process.env.MONGODB_URI,{useNewUrlParser:true , useFindAndModify: false, useCreateIndex: true, useUnifiedTopology: true })
 .then(() => {
         console.log('Conexión Con MongoDB Establecida');
         app.listen(port, () => {
